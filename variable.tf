5669ac80f5461681688d88472559561811ab4798
Aasishaws <115416682+Aasishaws@users.noreply.github.com>
2022-10-09 21:42:08 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,44 @@
+variable "region" {
+}
+variable "cidr_block" {
+}
+variable "instance_tenancy" {
+}
+variable "environment_name" {
+}
+variable "cidr_block_pub" {
+}
+variable "sub_pub" {
+}
+variable "cidr_block_prvt" {
+}
+variable "sub_prvt" {
+}
+variable "igw" {
+}
+variable "cidr_pubrt" {
+}
+variable "pubrt" {
+}
+variable "cidr_prvtrt" {
+}
+variable "prvtrt" {
+}
+variable "pub_ami" {
+}
+variable "pub_instance_type" {
+}
+variable "pub_key_name" {
+}
+variable "associate_pub" {
+}
+variable "prvt_ami" {
+}
+variable "prvt_instance_type" {
+}
+variable "prvt_key_name" {
+}
+variable "pubec2" {
+}
+variable "pvrtec2" {
+}
