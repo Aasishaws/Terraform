11f484a4bd7bd14b6a901c71226013f44b6a97d4
Aasishaws <115416682+Aasishaws@users.noreply.github.com>
2022-10-11 11:43:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "pubsub" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = var.cidr_block_pub
+  availability_zone = var.availability_zone_pubsub
 
   tags = {
     Name = var.sub_pub
@@ -19,6 +20,7 @@ resource "aws_subnet" "pubsub" {
 resource "aws_subnet" "pvtsub" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = var.cidr_block_prvt
+  availability_zone = var.availability_zone_prvtsub
 
   tags = {
     Name = var.sub_prvt
@@ -51,12 +53,29 @@ resource "aws_route_table_association" "pubsubassociate" {
   route_table_id = aws_route_table.pubrt.id
 }
 
+resource "aws_eip" "myeip" {
+  vpc      = true
+
+tags = {
+    Name = var.eip
+  }
+}
+
+resource "aws_nat_gateway" "mynat" {
+  allocation_id = aws_eip.myeip.id
+  subnet_id     = aws_subnet.pubsub.id
+
+  tags = {
+    Name = var.nat_gateway
+  }
+}
+
 resource "aws_route_table" "pvtrt" {
   vpc_id = aws_vpc.myvpc.id
 
   route {
       cidr_block = var.cidr_prvtrt
-      gateway_id = aws_internet_gateway.myigw.id
+      gateway_id = aws_nat_gateway.mynat.id
   }
 
   tags = {
@@ -68,13 +87,49 @@ resource "aws_route_table_association" "pvtsubassociate" {
   subnet_id      = aws_subnet.pvtsub.id
   route_table_id = aws_route_table.pvtrt.id
 }
+
+resource "aws_security_group" "allow_all" {
+  name        = var.pubsecuritygrp
+  description = var.pubsgdescription
+  vpc_id      = aws_vpc.myvpc.id
+
+ingress {
+      description      = var.ingressdescription_a
+      from_port        = var.ingressfromport_a
+      to_port          = var.ingresstoport_a
+      protocol         = var.ingressprotocol_a
+      cidr_blocks      = ["0.0.0.0/0"]
+  }
+ingress {
+      description      = var.ingressdescription_b
+      from_port        = var.ingressfromport_b
+      to_port          = var.ingresstoport_b
+      protocol         = var.ingressprotocol_b
+      cidr_blocks      = ["0.0.0.0/0"]
+  }
+egress {
+      description      = var.egressdescription
+      from_port        = var.egressfromport
+      to_port          = var.egresstoport
+      protocol         = var.egressprotocol
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+  }
+ tags = {
+    Name = var.pubsg
+  }
+}
+
+
 resource "aws_instance" "pubec2" {
   ami                         =  var.pub_ami
+  count                       =   var.pubec2count
   instance_type               =  var.pub_instance_type
   subnet_id                   =  aws_subnet.pubsub.id
   key_name                    =  var.pub_key_name
   associate_public_ip_address =  var.associate_pub
-
+  availability_zone           =  var.availability_zone_pubec2
+  vpc_security_group_ids      =  [aws_security_group.allow_all.id]
   tags = {
     Name = var.pubec2
   }
@@ -82,12 +137,15 @@ resource "aws_instance" "pubec2" {
 
 resource "aws_instance" "pvtec2" {
   ami                         =  var.prvt_ami
+  count                       =   var.prvtec2count
   instance_type               =  var.prvt_instance_type
   subnet_id                   =  aws_subnet.pvtsub.id
   key_name                    =  var.prvt_key_name
-
+  availability_zone           =  var.availability_zone_prvtec2
+  vpc_security_group_ids      =  [aws_security_group.allow_all.id]
 
   tags = {
     Name = var.pvrtec2
   }
 }
+
